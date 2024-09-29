d5c18da3f81b7bb8ea82d8d98d72ce995b5b72a2
Sylvia Oleander-Rothove <sylmarssbm@gmail.com>
2024-09-29 15:25:07 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Kanna's Quality of Life
+
+My friend gave me a list of things to improve the flow of gameplay. So I did.
+
+This mod provides a list of toggleable quality of life changes (Everyone say thank you prod.). You can pick and choose the settings for all of these with the config
+
+## Quality of Life changes
+
+* Instant Scrapping and Printing
+* Scrapper(s) in Bazaar (up to however many players there are)
+* Ping Lunar Seers to see destination
+* Teleporter instantly finishes charging after boss is killed, with time adjusted
+* Guaranteed Cleansing Pool on Alphesian Sanctuary
+* Only pet the glass frog once for deep void portal (cost configurable!)
+
+## Issues
+
+If you run into any issues while playing that this is mod is causing you feel free to report it on the [repo](https://github.com/SylmarDev/KannasQualityOfLife) using the issues tab, or ping me in the RoR2 modding discord, my username is PrincessSylmar.
+
+## TODO
+
+* Testing (hahaha.. good one)
+* Add Pinging Red Chests / Newt Altars on stage start
+* Rebalancing mobility items to appear as often as they did relative to when the game came out. Something I'm considering just for my runs.
+* More to come soon on this? We'll see :)
+
+### Special Thanks
+* prod (Making config files work)
+* Kanna
+* Kevin (Scrapper in shop implementation)
+* mrchous & Arbition (formulas for tele charge skip)
+
+## Changelog
+
+**0.2.2**
+* SOTS Update
+
+**0.2.1**
+* Fixed crash on startup
+
+**0.2.0**
+* Config files now enabled (shoutouts to prod)
+* Fixed bug with lunar seers looking a little weird
+
+**0.1.0**
+* Mostly done, just need to add pinged items at stage start
---
+++ RoR2-BossRush/Main.cs
@@ -0,0 +1,75 @@
+﻿using BepInEx;
+using R2API;
+using R2API.Utils;
+using RoR2;
+using UnityEngine;
+using System;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+using EntityStates;
+using MonoMod.Cil;
+using Mono.Cecil.Cil;
+using EntityStates.Duplicator;
+using UnityEngine.AddressableAssets;
+using Microsoft.VisualBasic;
+using SylmarDev.RoR2BossRush;
+using System.IO;
+using System.Numerics;
+using System.Resources;
+
+namespace SylmarDev.KannasQoL
+{
+    [BepInDependency(DirectorAPI.PluginGUID)]
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod)]
+
+    public class KannasQoL : BaseUnityPlugin
+    {
+        public const string PluginAuthor = "SylmarDev";
+        public const string PluginName = "RoR2BossRush";
+        public const string PluginGUID = PluginAuthor + "." + PluginName;
+        public const string PluginVersion = "0.3.2";
+
+        //The Awake() method is run at the very start when the game is initialized.
+        public void Awake()
+        {
+            //Init our logging class so that we can properly log for debugging
+            Log.Init(Logger);
+
+            Log.LogInfo($"{PluginGUID} // ver {PluginVersion}");
+
+            Log.LogInfo("Assigning hooks. . .");
+
+            On.RoR2.VoidRaidGauntletController.SpawnOutroPortal += SpawnOutroPortal_Hook;
+
+            // This line of log will appear in the bepinex console when the Awake method is done.
+            Log.LogInfo(nameof(Awake) + " done.");
+        }
+
+        private void SpawnOutroPortal_Hook(On.RoR2.VoidRaidGauntletController.orig_SpawnOutroPortal orig, VoidRaidGauntletController self)
+        {
+            orig(self);
+
+            // spawn Prime Meridian portal
+            if (NetworkServer.active && self.currentDonut != null && self.currentDonut.returnPoint)
+            {
+                Xoroshiro128Plus rng = new Xoroshiro128Plus(self.rngSeed + 1UL);
+                DirectorPlacementRule placementRule = new DirectorPlacementRule
+                {
+                    placementMode = DirectorPlacementRule.PlacementMode.Approximate,
+                    minDistance = self.minOutroPortalDistance,
+                    maxDistance = self.maxOutroPortalDistance,
+                    spawnOnTarget = self.currentDonut.returnPoint
+                };
+
+                SpawnCard primeMeridianPortalCard = Addressables.LoadAssetAsync<SpawnCard>("RoR2/DLC2/iscColossusPortal.asset").WaitForCompletion();
+
+                DirectorSpawnRequest directorSpawnRequest = new DirectorSpawnRequest(primeMeridianPortalCard, placementRule, rng);
+                DirectorCore.instance.TrySpawnObject(directorSpawnRequest);
+
+                // force portal to take you to Prime Meridian so you don't have to seek the whole storm
+                Stage.instance.sceneDef.stageOrder = 3;
+            }
+        }
+    }
+}
